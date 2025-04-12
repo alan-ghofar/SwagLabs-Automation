4571230e86981399be5f144d02f50dcee20e7bcb
allatifunalanghofar <l200204230@student.ums.ac.id>
2025-04-12 16:18:35 +0700

    file Login

--- src/main/java/config/env_target.java
+++ src/main/java/config/env_target.java
@@ -1,4 +1,8 @@
 package config;
 
+import org.openqa.selenium.WebDriver;
+
 public class env_target {
+    protected WebDriver driver;
+    protected String urlSwagLabs = "https://www.saucedemo.com/";
 }
--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import config.env_target;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Login {
+import java.time.Duration;
+
+public class Login extends env_target {
+    @Test
+    public void main(){
+        //set driverlocation path
+        System.setProperty("webdriver.microsoft-edge.driver","F:\\QA Testing\\untitled\\src\\main\\resources\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        //maximize driver
+        driver.manage().window().maximize();
+        //Set Url
+        driver.get(urlSwagLabs);
+
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+
+        wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
+        wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@class=\"title\"]"))
+        );
+
+        driver.quit();
+    }
+
+    @Test
+    public void fail(){
+        //set driverlocation path
+        System.setProperty("webdriver.microsoft-edge.driver","F:\\QA Testing\\untitled\\src\\main\\resources\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        //maximize driver
+        driver.manage().window().maximize();
+        //Set Url
+        driver.get(urlSwagLabs);
+
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+
+        wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauc");
+        driver.findElement(By.xpath("//*[@id=\'login-button\']")).click();
+
+        wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+
+        driver.quit();
+    }
 }
